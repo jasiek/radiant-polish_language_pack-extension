898b84c38881782a7281ca38c5aee57e41fd86cf
Jan Szumiec <jan.szumiec@gmail.com>
2010-06-28 14:24:40 +0200

    Rozszerzenie się ładuje i migruje. Zmieniłem nazwę rozszerzenia (po raz kolejny). Poprawiony plik README.

---
+++ pl_language_pack_extension.rb
@@ -0,0 +1,9 @@
+# -*- coding: undecided -*-
+class PlLanguagePackExtension < Radiant::Extension
+  version "1.0"
+  description "Projekt udostępnia polskie tłumaczenie interfejsu użytkownika administratora w Radiancie"
+  url "http://github.com/jasiek/radiant-polish_language_pack-extension"
+    
+  def activate
+  end
+end
